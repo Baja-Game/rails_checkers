535c4292f3a10bb31e008f7a4e3eb27a834ff0a5
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-18 14:49:39 -0500

    list method on games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,10 +11,15 @@ class GamesController < ApplicationController
                    [2, 0, 2, 0, 2, 0, 2, 0]]
 
   def create
-    @user = User.find_by(authentication_token: params[:auth_token] )
     @game = Game.create(board: INITIAL_BOARD, turn_counter: 1)
-    @game.game_users.create(user_id: @user.id)
+    @game.game_users.create(user_id: current_user.id)
     render json: { game: @game }, status: :ok
+  end
+
+  def list
+#    @user = User.find_by(authentication_token: params[:auth_token])
+    @games = current_user.games
+    render json: { game: @games }, status: :ok
   end
 
   def show
