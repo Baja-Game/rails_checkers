4880d1786a9a335f24fae8f898eb4a0e2a0e8afa
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-18 12:38:32 -0500

    create in games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,25 @@
 class GamesController < ApplicationController
   before_action :authenticate_user_from_token!
 
+  INITIAL_BOARD = [[0, 1, 0, 1, 0, 1, 0, 1],
+                   [1, 0, 1, 0, 1, 0, 1, 0],
+                   [0, 1, 0, 1, 0, 1, 0, 1],
+                   [0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0],
+                   [2, 0, 2, 0, 2, 0, 2, 0],
+                   [0, 2, 0, 2, 0, 2, 0, 2],
+                   [2, 0, 2, 0, 2, 0, 2, 0]]
+
+  def create
+ #   binding.pry
+    @user = User.find_by(authentication_token: params[:auth_token] )
+    @game = Game.create(board: INITIAL_BOARD, turn_counter: 1)
+    @game.game_users.create(user_id: @user.id)
+    render json: { game: @game }, status: :ok
+  end
+
   def show
     @game = Game.find(params[:id])
-    render json: { :game => @game }, status: :ok
+    render json: { game: @game }, status: :ok
   end
 end
