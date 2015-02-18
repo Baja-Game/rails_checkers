80c5f9835fbd59b41f698dfcf3b3cdb75ad06147
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-18 16:13:27 -0500

    edits to list

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'games/:id',        to: 'games#show', as: 'game'
   post 'games',           to: 'games#create'
   get 'games', to: 'games#list', as: 'game_list'
+  put 'games/:id', to: 'games#move'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
