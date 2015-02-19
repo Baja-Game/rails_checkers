4d70b9ff86e890370f74f6f2ad75720fa6d818bd
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-19 15:01:00 -0500

    move in controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,6 +12,17 @@ class GamesController < ApplicationController
     render json: { game: @games }, status: :ok
   end
 
+  def move
+    @game = Game.find(params[:id])
+    move = params[:move]
+    piece = @game.valid_piece(move.shift)
+    if piece
+      render json: { message: "Valid Move! Congrats"}, status: :ok
+    else
+      render json: { message: "Invalid Piece" }, status: :bad_request
+    end
+  end
+
   def show
     @game = Game.find(params[:id])
     render json: show_results(@game), status: :ok
