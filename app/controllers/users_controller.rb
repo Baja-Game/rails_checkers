9a9296c806f24254469cfeca299307be05d60284
Chubes <Chubes@andrewmsmacbook.att.net>
2015-02-21 09:44:58 -0500

    leaderboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.order(experience: :desc)
+    render json: @users, status: :ok
+  end
+
   def show
     #binding.pry
     @user = User.find(params[:id]) 
