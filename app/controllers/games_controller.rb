3f226bbaf95e2a93855ad29aec475eeea1bbec33
Alex McConnell <adm3333@yahoo.com>
2015-02-22 17:19:18 -0500

    force multiple jumps complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,12 +13,12 @@ class GamesController < ApplicationController
 
   def move
     @game = Game.find(params[:id])
-    params[:move] == "forfeit" ? moves = "forfeit" : moves = JSON.parse(params[:move])
+    params[:move] == 'forfeit' ? moves = 'forfeit' : moves = JSON.parse(params[:move])
     @game.log << moves
     @game.player1 ? valid_user = @game.users[0] : valid_user = @game.users[1]
     if valid_user != current_user
-      render json: { message: "Invalid Player" }, status: :bad_request
-    elsif moves == "forfeit"
+      render json: { message: 'Invalid Player' }, status: :bad_request
+    elsif moves == 'forfeit'
       @game.forfeit
       @game.end_turn
       render json: { message: "Player #{@game.finished} Wins By Forfeit"}, status: :ok
@@ -28,7 +28,7 @@ class GamesController < ApplicationController
       if piece
         if @game.valid_move?(piece).include?(moves[0])
           if @game.forced_jumps? && @game.check_jumps.size > 0
-            result = "jump"
+            result = 'jump'
           else  
             @game.process_move(piece, moves[0])
           end
@@ -42,24 +42,27 @@ class GamesController < ApplicationController
               @game.capture_counter = 40
               piece = m
             else
-              render json: { message: "Invalid Move" }, status: :bad_request
-              break
+              result = 'invalid'
             end  
           end
-          #check to force multiple jumps here
+          result = 'jump' if @game.valid_move?(moves[-1], true).size > 0
         end
         @game.end_turn unless result
         if @game.finished
           render json: { message: "Player #{@game.finished} Wins"}, status: :ok
         else
-          if result        
-            render json: { message: "Player Must Jump" }, status: :bad_request
+          if result 
+            if result == 'jump'       
+              render json: { message: 'Player Must Jump' }, status: :bad_request
+            else
+              render json: { message: 'Invalid Move' }, status: :bad_request
+            end
           else
-            render json: { message: "Move Successful"}, status: :ok
+            render json: { message: 'Move Successful' }, status: :ok
           end
         end
       else
-        render json: { message: "Invalid Piece" }, status: :bad_request
+        render json: { message: 'Invalid Piece' }, status: :bad_request
       end
     end
   end
